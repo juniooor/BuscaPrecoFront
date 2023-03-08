94cb78bc14a44efb8822b1be71c3d1a405e83192
Juniooor <juniormodern@hotmail.com>
2023-03-07 21:21:53 -0300

    definindo rotas

--- project_busca/app_price_search/views.py
+++ project_busca/app_price_search/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def pesquisar(requests):
+    return render(requests,'produtos/pesquisa.html')
+
+def exibir_resultados(requests):
+    pass
--- project_busca/project_busca/urls.py
+++ project_busca/project_busca/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from app_price_search import views
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # url para buscapreco.com
+    path('', views.pesquisar, name='pesquisar'),
+    # url para buscapreco.com/resultados
+    path('resultados/', views.exibir_resultados, name='resultados')
 ]
