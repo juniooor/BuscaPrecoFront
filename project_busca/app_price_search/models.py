b61fa532553342b6a0816f595d3daeaf44bcf867
Juniooor <juniormodern@hotmail.com>
2023-03-08 22:30:56 -0300

    criando models

--- project_busca/app_price_search/models.py
+++ project_busca/app_price_search/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    id_product = models.AutoField(primary_key=True)
+    name = models.TextField(max_length=255)
+    price = models.DecimalField(decimal_places=2, max_digits=12)
+    site = models.TextField(max_length=500)
+    quote_date = models.DateField()
+    link_image = models.TextField(max_length=500,
+                                  default='https://via.placeholder.com/150')
